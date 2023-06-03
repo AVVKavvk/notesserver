4ecd6bf40603270dd0223b08f744588e48e315c1
AVVKavvk <kumawatvipin066@gmail.com>
2023-06-03 17:29:31 +0530

    n

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,8 @@ const logger = require("../middleware/Logger");
 
 const route = require("express").Router();
 
-// route.post("/notes", logger, userControllers);
-// route.post("/labs", logger, userControllers);
-// route.post("/paper", logger, userControllers);
+route.post("/notes", logger, userControllers);
+route.post("/labs", logger, userControllers);
+route.post("/paper", logger, userControllers);
 
 module.exports = route;
