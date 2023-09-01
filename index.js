6586eeeedc4821b4775dde26c4e07a7b899ce03a
AVVKavvk <kumawatvipin066@gmail.com>
2023-09-01 16:32:12 +0530

    4003

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 Dbconnect();
-app.listen(3003, () => {
-  console.log("Server is running on port 3003");
+app.listen(4003, () => {
+  console.log("Server is running on port 4003");
 });
