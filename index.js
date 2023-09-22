c318bd51918d23b42db552279810b0b504bdd6cf
AVVKavvk <kumawatvipin066@gmail.com>
2023-09-22 13:23:23 +0530

    l

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 Dbconnect();
-app.listen(4007, () => {
-  console.log("Server is running on port 4007");
+app.listen(4003, () => {
+  console.log("Server is running on port 4003");
 });
 // app.listen(3001, () => {
 //   console.log("Server is running on port 3001");
