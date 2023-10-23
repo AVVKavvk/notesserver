fa8e59498e40cf8f190a28d1bc86a184ef6be7e0
AVVKavvk <kumawatvipin066@gmail.com>
2023-10-23 15:30:06 +0530

    botdata

--- bot.js
+++ bot.js
@@ -1,34 +1,45 @@
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 const bot = new Telegraf("6782667256:AAGWS0pas1NGKHXFVQ--5r9kIct-7MOEc6Y");
-
+const botArray=require('./assets/bot.json')
+const axios = require("axios");
 module.exports= async ()=>{
-    bot.start(ctx => ctx.reply("Welcome to your bot!"));
-
-    // Handle incoming messages
-    bot.on("text", ctx => {
-      const chatId = ctx.message.chat.id;
-      const messageText = ctx.message.text;
-    
-      // Respond to user messages
-      if (
-        messageText.toLowerCase() === "hi" ||
-        messageText.toLowerCase() === "hello"
-      ) {
-        ctx.reply("vipin");
-      } else {
-        ctx.reply("I am a simple bot. You can say hello or ask for assistance.");
-      }
+  bot.start(ctx => {
+    let welcomeMessage =
+      "Welcome to the DSA and C++ Algorithms Bot!\n\n" +
+      "This bot provides you with DSA (Data Structures and Algorithms) algorithms and C++ code for the most important problems.\n\n" +
+      "To get started, simply type any commands start with '/' or click on given command :\n\n";
+  
+    // Group the commands in rows of 4
+    for (let i = 0; i < botArray.length; i += 4) {
+      const commandsInRow = botArray
+        .slice(i, i + 4)
+        .map(item => `/${item.title}`)
+        .join("\t\t\t");
+  
+      welcomeMessage += commandsInRow + "\n\n";
+    }
+  
+    ctx.reply(welcomeMessage);
+  });
+  
+  bot.on("sticker", ctx => {
+    ctx.reply("You sent a sticker! 👍");
+  });
+  {
+    botArray?.map(item => {
+      bot.command(`${item?.title}`, async function (ctx) {
+        const response = await axios(`${item?.code}`);
+        ctx.reply(response?.data);
+        ctx.reply("/start");
+        return;
+      });
     });
-    
-    // Handle stickers
-    bot.on("sticker", ctx => {
-      ctx.reply("You sent a sticker! 👍");
-    });
-    
-    // Start the bot and enable graceful stop
-    bot.launch();
-    process.once("SIGINT", () => bot.stop("SIGINT"));
-    process.once("SIGTERM", () => bot.stop("SIGTERM"));
-    
+  }
+  
+  // Start the bot and enable graceful stop
+  bot.launch();
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+  
 }
