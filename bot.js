d2a5dfa7485b11d5962c2b9b3e5ba8a8cc90adfc
AVVKavvk <kumawatvipin066@gmail.com>
2023-10-26 11:46:09 +0530

    bot

--- bot.js
+++ bot.js
@@ -1,28 +1,28 @@
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 const bot = new Telegraf("6782667256:AAGWS0pas1NGKHXFVQ--5r9kIct-7MOEc6Y");
-const botArray=require('./assets/bot.json')
+const botArray = require("./assets/bot.json");
 const axios = require("axios");
-module.exports= async ()=>{
+module.exports = async () => {
   bot.start(ctx => {
     let welcomeMessage =
       "Welcome to the DSA and C++ Algorithms Bot!\n\n" +
       "This bot provides you with DSA (Data Structures and Algorithms) algorithms and C++ code for the most important problems.\n\n" +
       "To get started, simply type any commands start with '/' or click on given command :\n\n";
-  
+
     // Group the commands in rows of 4
     for (let i = 0; i < botArray.length; i += 4) {
       const commandsInRow = botArray
         .slice(i, i + 4)
         .map(item => `/${item.title}`)
         .join("\t\t\t");
-  
+
       welcomeMessage += commandsInRow + "\n\n";
     }
-  
+
     ctx.reply(welcomeMessage);
   });
-  
+
   bot.on("sticker", ctx => {
     ctx.reply("You sent a sticker! 👍");
   });
@@ -38,10 +38,9 @@ module.exports= async ()=>{
       });
     });
   }
-  
+
   // Start the bot and enable graceful stop
   bot.launch();
   process.once("SIGINT", () => bot.stop("SIGINT"));
   process.once("SIGTERM", () => bot.stop("SIGTERM"));
-  
-}
+};
