269a70e0a0dbacc20e4eed1087ced30d2bd58a08
AVVKavvk <kumawatvipin066@gmail.com>
2024-11-08 13:37:21 +0530

    otp issue

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -9,9 +9,13 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTPEmail,
     pass: process.env.SMTPPass,
   },
+  debug: true, // Enable debug mode
+  // logger: true,
 });
 const sendEmailController = expressAsyncHandler(async (req, res) => {
   const email = req?.body?.email;
+  // console.log(email);
+
   const otp = otpGenerator.generate(6, {
     upperCaseAlphabets: true,
     specialChars: true,
@@ -19,9 +23,9 @@ const sendEmailController = expressAsyncHandler(async (req, res) => {
   var mailOptions = {
     from: process.env.SMTPEmail,
     to: email,
-    subject: "OTP from VipinNotes! Ultimate Academic, Career, and Personal Growth Platform for IIIT Pune Students",
+    subject:
+      "OTP from VipinNotes! Ultimate Academic, Career, and Personal Growth Platform for IIIT Pune Students",
     text: `Your OTP is: ${otp} \n\nThanks for choosing VipinNotes\nRegards,\nVipin Kumawat`,
-    
   };
 
   transporter.sendMail(mailOptions, function (err, info) {
